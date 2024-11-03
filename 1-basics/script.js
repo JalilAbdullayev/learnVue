19783f9819b24b275f49d40deccb1d2686dbc83d
Jalil <abdullacelil00@gmail.com>
2024-11-03 11:58:26 +0400

    Event binding

--- 1-basics/script.js
+++ 1-basics/script.js
@@ -1,21 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstGoal: "HTML",
-            secondGoal: "<h3>CSS</h3>",
-            thirdGoal: "JS",
-            website: "https://www.google.com/",
+            counter: 0
         };
-    },
-    methods: {
-        ourGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.4) {
-                return this.secondGoal;
-            } else {
-                return this.thirdGoal;
-            }
-        },
-    },
+    }
 });
 app.mount("#frontend");
