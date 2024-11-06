17dedc9cf86649c17b6004b916239b7df7439c1c
Jalil <abdullacelil00@gmail.com>
2024-11-06 11:01:52 +0400

    Create a student list component

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,34 +1,12 @@
 <template>
-  <h2>
-    My students
-  </h2>
-  <ul>
-    <li v-for="student in students" :key="student.name">
-      {{ student.name }} - {{ student.phone }}
-    </li>
-  </ul>
+  <StudentList/>
 </template>
 
 <script>
+import StudentList from "@/components/StudentList.vue";
+
 export default {
   name: 'App',
-  data() {
-    return {
-      students: [
-        {
-          name: 'Jalil Abdullayev',
-          phone: '+994104154315'
-        },
-        {
-          name: 'Jack Martin',
-          phone: '+994514578964'
-        },
-        {
-          name: 'John Doe',
-          phone: '+994554987654'
-        }
-      ]
-    };
-  }
+  components: {StudentList}
 };
 </script>
---
+++ first-project/src/components/StudentList.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "StudentList",
+  data() {
+    return {
+      students: [{
+        name: 'Jalil Abdullayev',
+        phone: '+994104154315'
+      }, {
+        name: 'Jack Martin',
+        phone: '+994514578964'
+      }, {
+        name: 'John Doe',
+        phone: '+994554987654'
+      }]
+    };
+  }
+};
+</script>
+
+<template>
+  <h2>
+    My students
+  </h2>
+  <ul>
+    <li v-for="student in students" :key="student.name">
+      {{ student.name }} - {{ student.phone }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
